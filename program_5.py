44fd581e7d283889a88abc98b56cd3dbc67e8f20
Trevor Conger <149925363+trevcong@users.noreply.github.com>
2024-09-19 17:34:37 -0500

    Program 5 should be working fine.

--- program_5.py
+++ program_5.py
@@ -4,3 +4,58 @@
 # Also tax is 7%. 
 # Write a program the inputs the type of hot dog wanted and optional toppings.  
 # The program then displays the hot dog cost, tax and total cost. 
+
+def orderFunction():
+    ordercostTaxAndTotalCost = []
+    print("Hello and welcome to Trevors Hot Dogs! \n")
+    print("MENU")
+    print("COMBO 1 : HOT DOG__________________$3.50")
+    print("COMBO 2 : CHILI DOG________________$4.50")
+    valueForTrue = True
+    while(valueForTrue):
+        try:
+            userSelectionForHotDog = int(input("So what would you like? Combo 1 or 2?"))
+            if userSelectionForHotDog == 1:
+                ordercostTaxAndTotalCost.append(3.50) 
+                print("Great choice! Can't go wrong with a plain dog while watching the Twin's play! \n")
+                doYouWantCheese = input("Do you want cheese on your hot dog? (yes or no) \n").lower().strip()
+                if doYouWantCheese == "yes":
+                    ordercostTaxAndTotalCost[0] = ordercostTaxAndTotalCost[0] + .50
+                doYouWantPeppers = input("Do you want peppers on your hot dog? (yes or no) \n").lower().strip()
+                if doYouWantPeppers == "yes":
+                    ordercostTaxAndTotalCost[0] = ordercostTaxAndTotalCost[0] + .75
+                doYouWantGrilledOnions = input("Do you want peppers on your hot dog? (yes or no) \n")
+                if doYouWantGrilledOnions == "yes":
+                    ordercostTaxAndTotalCost[0] = ordercostTaxAndTotalCost[0] + 1.00
+                ordercostTaxAndTotalCost.append((ordercostTaxAndTotalCost[0] * 0.07))
+                ordercostTaxAndTotalCost.append(ordercostTaxAndTotalCost[0] + ordercostTaxAndTotalCost[1])
+                break
+                
+            if userSelectionForHotDog == 2:
+                ordercostTaxAndTotalCost.append(4.50)
+                print("Great choice! Can't go wrong with a chili dog while watching the Twin's play! \n")
+                doYouWantCheese = input("Do you want cheese on your chili dog? (yes or no) \n").lower().strip()
+                if doYouWantCheese == "yes":
+                    ordercostTaxAndTotalCost[0] = ordercostTaxAndTotalCost[0] + .50
+                doYouWantPeppers = input("Do you want peppers on your chili dog? (yes or no) \n").lower().strip()
+                if doYouWantPeppers == "yes":
+                    ordercostTaxAndTotalCost[0] = ordercostTaxAndTotalCost[0] + .75
+                doYouWantGrilledOnions = input("Do you want peppers on your chili dog? (yes or no) \n")
+                if doYouWantGrilledOnions == "yes":
+                    ordercostTaxAndTotalCost[0] = ordercostTaxAndTotalCost[0] + 1.00
+                ordercostTaxAndTotalCost.append((ordercostTaxAndTotalCost[0] * 0.07))
+                ordercostTaxAndTotalCost.append(ordercostTaxAndTotalCost[0] + ordercostTaxAndTotalCost[1])
+                break
+            else:
+                continue
+        except ValueError:
+            print("Thats not a selection..")
+
+    return ordercostTaxAndTotalCost
+
+
+if __name__ == '__main__':
+    order = orderFunction()
+    print("Your hot dog will cost you $" + str(order[0]))
+    print("Your tax will be $" + str(order[1]))
+    print("Your final cost will come to $ " + str(order[2]))
